b0dbde1e63f0589832ef17591c69314e3d3b3c1f
Artem Rimarchuk <1patrik@mail.ru>
2020-02-23 16:12:43 +0300

    solve 3 task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,8 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let arr = members.filter(member => typeof (member) == 'string')
+    let str = arr.map(item => item.trim()[0].toUpperCase()).sort().join('');
+    return str
+  }
+  return false;
 };
