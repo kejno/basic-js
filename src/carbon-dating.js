3a8b2f9205cc5b5cdfb8c43782cb7e4cfc7284a8
Artem Rimarchuk <1patrik@mail.ru>
2020-02-23 15:34:45 +0300

    fix: edit 2 tasts

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,8 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(act) {
-  let year = Math.ceil(Math.log(MODERN_ACTIVITY / +act) / 0.693 * HALF_LIFE_PERIOD)
 
-  return typeof (act) == 'string' && +act / 1 == (+act) && +act > 0 && +act < 15 ? year : false;
+  let year = Math.ceil(Math.log(MODERN_ACTIVITY / parseFloat(act)) / 0.693 * HALF_LIFE_PERIOD)
+
+  return typeof (act) == 'string' && parseFloat(act) / 1 == (parseFloat(act)) && parseFloat(act) > 0 && parseFloat(act) < 15 ? year : false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-module.exports = function countCats(backyard) {
+/* module.exports = function countCats(backyard) {
   let count = 0;
   for (let i = 0; i < backyard.length; i++) {
     for (let k = 0; k < backyard[i].length; k++) {
@@ -8,4 +8,13 @@ module.exports = function countCats(backyard) {
     }
   }
   return count;
+}; */
+
+module.exports = function countCats(backyard) {
+  let arr = [];
+  for (let i = 0; i < backyard.length; i++) {
+    arr.push(...backyard[i])
+  }
+  let res = arr.filter(item => item == "^^")
+  return res.length;
 };
