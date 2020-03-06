1c45a964af5c51e503e48023820e7c832e48f7a5
Artem Rimarchuk <1patrik@mail.ru>
2020-03-07 01:32:28 +0300

    feat: add hanoi-tower, what-season, transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,30 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) {
+        throw new Error();
+    }
+    else if (arr.length == 0) {
+        return [];
+    }
+
+    let res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === "--discard-prev") {
+            arr[i - 1] !== undefined ? res.pop() : null
+        }
+
+        else if (arr[i] === "--discard-next") i++;
+
+        else if (arr[i] === "--double-prev") {
+            arr[i - 1] !== undefined ? res.push(arr[i - 1]) : null
+        }
+
+        else if (arr[i] === "--double-next") {
+            arr[i + 1] !== undefined ? res.push(arr[i + 1]) : null
+        }
+
+        else res.push(arr[i])
+    }
+
+    return res
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,27 +1,26 @@
 module.exports = function getSeason(date) {
+
+  if (!date) {
+    return "Unable to determine the time of year!";
+  } else if (isNaN(date.valueOf())) throw new Error;
+
   let season = date.getMonth();
-  if (date) {
-    let season = date.getMonth();
-
-    switch (season) {
-      case 11:
-      case 0:
-      case 1:
-        return 'winter';
-      case 2:
-      case 3:
-      case 4:
-        return 'spring';
-      case 5:
-      case 6:
-      case 7:
-        return 'summer';
-      case 8:
-      case 9:
-      case 10:
-        return 'autumn';
-    }
+  switch (season) {
+    case 11:
+    case 0:
+    case 1:
+      return 'winter';
+    case 2:
+    case 3:
+    case 4:
+      return 'spring';
+    case 5:
+    case 6:
+    case 7:
+      return 'summer';
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn';
   }
-  return 'Unable to determine the time of year!'
-
-};
+}
